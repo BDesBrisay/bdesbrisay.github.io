75baea7c62ed19a5baf0b012c4c7aeb29f002ff9
Bryce DesBrisay <bryce@desbrisay.com>
2020-01-05 03:27:40 -0800

    added more logos, started on header

--- history.js
+++ history.js
@@ -1,22 +1,19 @@
 window.onscroll = (e) => {
-  console.log(e, e.deltaY, e.wheelData)
-
-
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   var scrolled = (winScroll / height) * 100;
 
-  // const percent = scrolled + 9;
   document.getElementById('progressBar').style.height = scrolled + '%';
 
-  const ints = [0, 6, 14, 25, 33.3, 41.7, 50, 58.3, 69, 77, 86, 95];
-  // const ints = [0, 9, 18, 27, 36, 45, 54, 63, 72, 81, 91, 100];
-  // const closest = ints.reduce((prev, curr) => Math.abs(curr - scrolled) < Math.abs(prev - scrolled) ? curr : prev);
+  const ints = [0, 6, 14, 22, 30, 38, 46, 54, 62, 70, 78, 86, 94];
   const closest = ints.reduce((prev, curr) => scrolled > curr ? curr : prev);
+  const index = 13 - ints.indexOf(closest);
 
-  const index = 12 - ints.indexOf(closest);
-
-  const link = document.getElementById(`to${index}`)
-
-  link.className = 'before';
+  let i = 1;
+  while (i < 13) {
+    const link = document.getElementById(`to${i}`);
+    if (i >= index) link.className = 'before';
+    else link.className = '';
+    i++;
+  }
 }
