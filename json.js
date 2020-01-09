bbb096217e123ad64c50bb2e46829cc6e568efeb
Bryce DesBrisay <bryce@desbrisay.com>
2020-01-08 18:39:52 -0800

    json verson of resume

--- json.js
+++ json.js
@@ -1,3 +1,63 @@
+function htmlEscape(s) {
+  return s
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;');
+}
+
+function init() {
+  const json = document.getElementById('json');
+  for (let project of projects) {
+    json.insertAdjacentHTML('beforeend', printObject(project, 1));
+  }
+}
+
+function printObject(obj, level) {
+  let str = `<span class="i${level}">{</span><br/>`;
+
+  for (let key of Object.keys(obj)) {
+    const item = obj[key];
+
+    if (key !== 'imageStyle') {
+      str += `
+        <p class="i${level + 1}">
+          <b>${key}:</b>
+          ${key === 'tags' || key === 'links'
+            ? printArray(item, level + 1)
+            : key === 'image'
+              ? printImage(item, level + 1)
+              : key === 'title'
+                ? `<strong>${item}</strong>`
+                : key === 'link'
+                  ? `<a href="${item}">${item}</a>`
+                  : item
+          }
+        </p>
+      \n`;
+    }
+  }
+
+  str += `<span class="i${level}">}</span><br/>`;
+  return str;
+}
+
+function printArray(arr, level) {
+  let str = `<span>[</span></p>`;
+
+  for (let item of arr) {
+    typeof item === 'object'
+      ? str += printObject(item, level + 1)
+      : str += `<p class="i${level + 1}">${item}</p>`;
+  }
+
+  str += `<span class="i${level}">]</span><br/><p>`;
+  return str;
+}
+
+function printImage(src) {
+  return `<img src="${src}" alt="" />`;
+}
+
 const projects = [
   {
     image: './images/blazebuddies.png',
@@ -220,105 +280,4 @@ const projects = [
     ]
   }
 ];
-
-let activeTags = [];
-
-function init() {
-  const urlHash = window.location.hash;
-  activeTags = decodeURIComponent(urlHash).split('#').slice(1);
-
-  loadProjects();
-}
-
-function toggleTag(tag) {
-  const loc = window.location;
-  if (activeTags.includes(tag)) activeTags = activeTags.filter((i) => i !== tag);
-  else activeTags = [...activeTags, tag];
-
-  const extention = activeTags.length ? activeTags.map((tag) => `#${tag}`).join('') : '';
-  window.location = loc.origin + loc.pathname + extention;
-
-  clearProjects();
-  loadProjects();
-}
-
-function loadProjects() {
-  const projectsElement = document.getElementById('projects');
-  const filterElement = document.getElementById('filters');
-  let tags = [];
-
-  for (project of projects) {
-    const hasActiveTag = activeTags.every(val => project.tags.includes(val));
-
-    if (hasActiveTag || !activeTags.length) {
-      projectsElement.insertAdjacentHTML('beforeend', /*html*/`
-        <div class="project col">
-          <img src="${project.image}" alt="" class="logo" style="${project.imageStyle}" />
-          <h3 class="title">${project.title}</h3>
-          <h5 class="position">${project.position}</h5>
-          <p class="dates"><i>${project.dates}</i></p>
-          <p class="description">${project.description}</p>
-          <h6 class="subheader">Links</h6>
-          <div class="links row">
-            ${project.links.map((link) => `
-              <a
-                href="${link.link}"
-                class="link"
-                target="_blank"
-              >
-                ${link.label}
-              </a>
-            `).join('')}
-          </div>
-          <h6 class="subheader">Tags</h6>
-          <div class="tags">
-            ${project.tags.map((tag) => `
-              <a 
-                onclick="toggleTag('${tag}'); return false;" 
-                href="#${tag}"
-                class="noAfter"
-                ${activeTags.includes(tag) ? 'id="activeTag"' : ''}
-              >
-                ${tag}
-              </a>
-            `).join('')}
-          </div>
-        </div>
-      `);
-    }
-
-    tags = [...tags, ...project.tags];
-  }
-
-  const counts = {};
-  for (let tag of tags) counts[tag] 
-    ? counts[tag]++
-    : counts[tag] = 1;
-
-  const topKeys = Object.keys(counts).sort((a, b) => counts[b] - counts[a]);
-
-  filterElement.insertAdjacentHTML('beforeend', `
-    <div class="tags" id="filterTags">
-      ${topKeys.map((tag) => `
-        <a 
-          onclick="toggleTag('${tag}'); return false;" 
-          href="#${tag}"
-          ${activeTags.includes(tag) ? 'id="activeTag"' : ''}
-          data-count="${counts[tag]}"
-        >
-          ${tag}
-        </a>
-      `).join('')}
-    </div>
-  `);
-}
-
-function clearProjects() {
-  const projectsElement = document.getElementById('projects');
-  const filterElement = document.getElementById('filterTags');
-
-  while (projectsElement.firstChild) {
-    projectsElement.removeChild(projectsElement.firstChild);
-  }
-  filterElement.remove();
-}
+    
