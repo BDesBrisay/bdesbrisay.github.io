5bf5bd1a9a8b2a35481c65d9391dea73cef7cbfb
Bryce DesBrisay <bryce@desbrisay.com>
2020-01-15 04:14:26 -0700

    general tag types

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ const projects = [
     dates: 'January 2020 - Present',
     description: 'BlazeBuddies enables peer-to-peer, fully encrypted cannabis live streams and group video chats. I put together and styled the frontend client with a homemade single-page-application (SPA) framework using vanilla JavaScript, HTML, & CSS. I designed the logo as well as the overall UI and UX of the site.',
     tags: [
-      'Socket.io',
-      'Peer.js',
-      'Web Application',
-      'JavaScript, HTML, CSS',
-      'UI/UX',
-      'Graphic Design',
-      'SPA',
-      'Frontend'
+      { title: 'Socket.io', type: 'Technology' },
+      { title: 'Peer.js', type: 'Technology' },
+      { title: 'Web Application', type: 'Skill' },
+      { title: 'JavaScript, HTML, CSS', type: 'Language' },
+      { title: 'UI/UX', type: 'Skill' },
+      { title: 'Graphic Design', type: 'Skill' },
+      { title: 'SPA', type: 'Technology' },
+      { title: 'Frontend', type: 'Skill' }
     ],
     links: [
       {
@@ -31,17 +31,17 @@ const projects = [
     dates: 'October 2019 - December 2019',
     description: 'Donedingo is your private personal project planner based off of OKR and KPI techniques. This was a project for CSCI 3010 at the Univeristy of Colorado, Boulder during the semester of fall 2019. I built this project with a React.js frontend, CSS Modules for style, Firebase Auth for user accounts, Firebase Firestore for the database and storage, and custom unit tests for all database queries and mutations.',
     tags: [
-      'React.js',
-      'CSS Modules',
-      'Firebase Auth',
-      'Firebase Firestore',
-      'Web Application',
-      'JavaScript, HTML, CSS',
-      'Frontend',
-      'Backend',
-      'Graphic Design',
-      'UI/UX',
-      'SPA',
+      { title: 'React.js', type: 'Technology' },
+      { title: 'CSS Modules', type: 'Technology' },
+      { title: 'Firebase Auth', type: 'Technology' },
+      { title: 'Firebase Firestore', type: 'Technology' },
+      { title: 'Backend', type: 'Skill' },
+      { title: 'Web Application', type: 'Skill' },
+      { title: 'JavaScript, HTML, CSS', type: 'Language' },
+      { title: 'UI/UX', type: 'Skill' },
+      { title: 'Graphic Design', type: 'Skill' },
+      { title: 'SPA', type: 'Technology' },
+      { title: 'Frontend', type: 'Skill' }
     ],
     links: [
       {
@@ -57,18 +57,18 @@ const projects = [
     dates: 'July 2019 - Present',
     description: 'Soapely brings car washes and detailing to your driveway in 30 minutes or less. I desgined the logo, graphics, UI, & UX of the site and built the entire frontend in React.js & CSS Modules with a data layer of Apollo + GraphQL.',
     tags: [
-      'React.js',
-      'GraphQL',
-      'Apollo',
-      'Socket.io',
-      'Web Application',
-      'JavaScript, HTML, CSS',
-      'CSS Modules',
-      'UI/UX',
-      'Graphic Design',
-      'SPA',
-      'Frontend',
-      'Firebase Auth'
+      { title: 'React.js', type: 'Technology' },
+      { title: 'GraphQL', type: 'Technology' },
+      { title: 'Apollo', type: 'Technology' },
+      { title: 'Socket.io', type: 'Technology' },
+      { title: 'Web Application', type: 'Skill' },
+      { title: 'JavaScript, HTML, CSS', type: 'Language' },
+      { title: 'CSS Modules', type: 'Technology' },
+      { title: 'UI/UX', type: 'SKill' },
+      { title: 'Graphic Design', type: 'Skill' },
+      { title: 'SPA', type: 'Technology' },
+      { title: 'Frontend', type: 'Skill' },
+      { title: 'Firebase Auth', type: 'Technology' }
     ],
     links: [
       {
@@ -84,19 +84,19 @@ const projects = [
     dates: 'December 2018 - Present',
     description: 'Appearix is a single place to discover, create, and share events. I designed the logos, modified wireframes, styled the client, and built out the entire frontend interface in React.js, Apollo/GraphQL, CSS Modules. I also built out an entire app using React Native that was deployed to both the iOS app store and the Google Play store. Worked on event outreach with the administration at University of Colorado, Boulder and various clubs there too.',
     tags: [
-      'React.js',
-      'React Native',
-      'GraphQL',
-      'Apollo',
-      'Web Application',
-      'JavaScript, HTML, CSS',
-      'CSS Modules',
-      'iOS',
-      'Android',
-      'Mobile Application',
-      'SPA',
-      'UI/UX',
-      'Frontend'
+      { title: 'React.js', type: 'Technology' },
+      { title: 'React Native', type: 'Technology' },
+      { title: 'GraphQL', type: 'Technology' },
+      { title: 'Apollo', type: 'Technology' },
+      { title: 'Web Application', type: 'Skill' },
+      { title: 'JavaScript, HTML, CSS', type: 'Language' },
+      { title: 'CSS Modules', type: 'Technology' },
+      { title: 'iOS', type: 'Skill' },
+      { title: 'Android', type: 'Skill' },
+      { title: 'Mobile Application', type: 'Skill' },
+      { title: 'SPA', type: 'Technology' },
+      { title: 'UI/UX', type: 'Skill' },
+      { title: 'Frontend', type: 'Skill' }
     ],
     links: [
       {
@@ -116,17 +116,17 @@ const projects = [
     dates: 'October 2017 - Present',
     description: 'Presearch is an open, decentralized search engine that rewards community members with PRE, Presearch\'s cryptocurrency token, for their use of the engine. I built an entire frontend for this search engine using the bing API, Socket.io, React.js, CSS Modules, GraphQL, & Apollo. I also setup and built out a browser + search engine prototype in Electron and vanilla Javascript, HTML, and CSS. There is also a version with alternative results called Dsearch.com.',
     tags: [
-      'Electron',
-      'React.js',
-      'GraphQL',
-      'Apollo',
-      'Socket.io',
-      'Web Application',
-      'JavaScript, HTML, CSS',
-      'CSS Modules',
-      'SPA',
-      'UI/UX',
-      'Frontend'
+      { title: 'Electron', type: 'Technology' },
+      { title: 'React.js', type: 'Technology' },
+      { title: 'GraphQL', type: 'Technology' },
+      { title: 'Apollo', type: 'Technology' },
+      { title: 'Socket.io', type: 'Technology' },
+      { title: 'Web Application', type: 'Skill' },
+      { title: 'JavaScript, HTML, CSS', type: 'Language' },
+      { title: 'CSS Modules', type: 'Technology' },
+      { title: 'SPA', type: 'Technology' },
+      { title: 'UI/UX', type: 'Skill' },
+      { title: 'Frontend', type: 'Skill' }
     ],
     links: [
       {
@@ -146,19 +146,19 @@ const projects = [
     dates: 'July 2017 - July 2018',
     description: 'Magnify Progress is a political social network that allows you to take action today and make a difference in your local politics. Assisted in front-end development of React.js components and assembly of React Native iOS/Android app. Facilitated UX/UI and overall design of website and mobile app.',
     tags: [
-      'React.js',
-      'React Native',
-      'GraphQL',
-      'Apollo',
-      'Web Application',
-      'JavaScript, HTML, CSS',
-      'CSS Modules',
-      'iOS',
-      'Android',
-      'Mobile Application',
-      'SPA',
-      'UI/UX',
-      'Frontend'
+      { title: 'React.js', type: 'Technology' },
+      { title: 'React Native', type: 'Technology' },
+      { title: 'GraphQL', type: 'Technology' },
+      { title: 'Apollo', type: 'Technology' },
+      { title: 'Web Application', type: 'Skill' },
+      { title: 'JavaScript, HTML, CSS', type: 'Language' },
+      { title: 'CSS Modules', type: 'Technology' },
+      { title: 'iOS', type: 'Skill' },
+      { title: 'Android', type: 'Skill' },
+      { title: 'Mobile Application', type: 'Skill' },
+      { title: 'SPA', type: 'Technology' },
+      { title: 'UI/UX', type: 'Skill' },
+      { title: 'Frontend', type: 'Skill' }
     ],
     links: [
       {
@@ -182,14 +182,14 @@ const projects = [
     dates: 'August 2016 - February 2017',
     description: 'Real Identities help vendors check and verify the integrity of an ID card. Remodeled the user interface and assembled entire website from scratch. Used HTML, CSS, Javascript, and PHP in both design and development of website. Created SEO focused design and interface.',
     tags: [
-      'JavaScript, HTML, CSS',
-      'PHP',
-      'Frontend',
-      'Backend',
-      'SEO',
-      'Graphic Design',
-      'UI/UX',
-      'Web Application'
+      { title: 'JavaScript, HTML, CSS', type: 'Language' },
+      { title: 'PHP', type: 'Language' },
+      { title: 'Frontend', type: 'Skill' },
+      { title: 'Backend', type: 'Skill' },
+      { title: 'SEO', type: 'Skill' },
+      { title: 'Graphic Design', type: 'Skill' },
+      { title: 'UI/UX', type: 'Skill' },
+      { title: 'Web Application', type: 'Skill' }
     ],
     links: [
       {
@@ -205,12 +205,12 @@ const projects = [
     dates: 'January 2011 - August 2015',
     description: 'Harvix is a research engine designed to provide students with relevant data upfront. I developed the front-end aspects for a new web search engine for students. I recruited and collaborated successfully with an international team while acquiring partnerships with WolframAlpha, Seattle Public Schools, and Reddit. Organized meetings with Sequoia Union High School District about widespread implementation.',
     tags: [
-      'JavaScript, HTML, CSS',
-      'Web Application',
-      'Recruiting',
-      'Marketing',
-      'UI/UX',
-      'Graphic Design'
+      { title: 'JavaScript, HTML, CSS', type: 'Language' },
+      { title: 'Web Application', type: 'Skill' },
+      { title: 'Recruiting', type: 'Skill' },
+      { title: 'Marketing', type: 'Skill' },
+      { title: 'UI/UX', type: 'Skill' },
+      { title: 'Graphic Design', type: 'Skill' }
     ],
     links: [
       {
@@ -220,6 +220,33 @@ const projects = [
     ]
   }
 ];
+
+const tagsMap = {
+  'Socket.io': 'Technology',
+  'Peer.js': 'Technology',
+  'SPA': 'Technology',
+  'React.js': 'Technology',
+  'CSS Modules': 'Technology',
+  'Firebase Auth': 'Technology',
+  'Firebase Firestore': 'Technology',
+  'GraphQL': 'Technology',
+  'Apollo': 'Technology',
+  'React Native': 'Technology',
+  'Electron': 'Technology',
+  'Web Application': 'Skill',
+  'UI/UX': 'Skill',
+  'Graphic Design': 'Skill',
+  'Frontend': 'Skill' ,
+  'Backend': 'Skill',
+  'iOS': 'Skill',
+  'Android': 'Skill',
+  'Mobile Application': 'Skill',
+  'SEO': 'Skill',
+  'Recruiting': 'Skill',
+  'Marketing': 'Skill',
+  'PHP': 'Language',
+  'JavaScript, HTML, CSS': 'Language',
+}
 
 let activeTags = [];
 
@@ -248,7 +275,7 @@ function loadProjects() {
   let tags = [];
 
   for (project of projects) {
-    const hasActiveTag = activeTags.every(val => project.tags.includes(val));
+    const hasActiveTag = activeTags.every(val => project.tags.some(tag => tag.title === val));
 
     if (hasActiveTag || !activeTags.length) {
       projectsElement.insertAdjacentHTML('beforeend', /*html*/`
@@ -258,6 +285,19 @@ function loadProjects() {
           <h5 class="position">${project.position}</h5>
           <p class="dates"><i>${project.dates}</i></p>
           <p class="description">${project.description}</p>
+          <h6 class="subheader">Tags</h6>
+          <div class="tags">
+            ${project.tags.map((tag) => `
+              <a 
+                onclick="toggleTag('${tag.title}'); return false;" 
+                href="#${tag.title}"
+                class="noAfter"
+                ${activeTags.includes(tag.title) ? `id="${tagsMap[tag.title]}-tag"` : ''}
+              >
+                ${tag.title}
+              </a>
+            `).join('')}
+          </div>
           <h6 class="subheader">Links</h6>
           <div class="links row">
             ${project.links.map((link) => `
@@ -270,19 +310,6 @@ function loadProjects() {
               </a>
             `).join('')}
           </div>
-          <h6 class="subheader">Tags</h6>
-          <div class="tags">
-            ${project.tags.map((tag) => `
-              <a 
-                onclick="toggleTag('${tag}'); return false;" 
-                href="#${tag}"
-                class="noAfter"
-                ${activeTags.includes(tag) ? 'id="activeTag"' : ''}
-              >
-                ${tag}
-              </a>
-            `).join('')}
-          </div>
         </div>
       `);
     }
@@ -291,24 +318,45 @@ function loadProjects() {
   }
 
   const counts = {};
-  for (let tag of tags) counts[tag] 
-    ? counts[tag]++
-    : counts[tag] = 1;
+  for (let tag of tags) counts[tag.title] 
+    ? counts[tag.title]++
+    : counts[tag.title] = 1;
 
   const topKeys = Object.keys(counts).sort((a, b) => counts[b] - counts[a]);
 
+  const typeCounts = {};
+  for (let tag of topKeys) typeCounts[tagsMap[tag]]
+    ? typeCounts[tagsMap[tag]]++
+    : typeCounts[tagsMap[tag]] = 1;
+
+  const topTypes = Object.keys(typeCounts).sort((a, b) => typeCounts[b] - typeCounts[a]);
+
   filterElement.insertAdjacentHTML('beforeend', `
-    <div class="tags" id="filterTags">
-      ${topKeys.map((tag) => `
-        <a 
-          onclick="toggleTag('${tag}'); return false;" 
-          href="#${tag}"
-          ${activeTags.includes(tag) ? 'id="activeTag"' : ''}
-          data-count="${counts[tag]}"
-        >
-          ${tag}
-        </a>
-      `).join('')}
+    
+    <div id="filterTags">
+      <div class="tags">
+        ${topTypes.map((type) => `
+          <a 
+            class="tag" 
+            id="${type}-tag"
+            data-count="${typeCounts[type]}"
+          >
+            ${type}
+          </a>
+        `).join('')}
+      </div>
+      <div class="tags">
+        ${topKeys.map((tag) => `
+          <a 
+            onclick="toggleTag('${tag}'); return false;" 
+            href="#${tag}"
+            ${activeTags.includes(tag) ? `id="${tagsMap[tag]}-tag"` : ''}
+            data-count="${counts[tag]}"
+          >
+            ${tag}
+          </a>
+        `).join('')}
+      </div>
     </div>
   `);
 }
