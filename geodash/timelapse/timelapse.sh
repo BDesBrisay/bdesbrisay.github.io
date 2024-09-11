e63bc557ea30d2f51ab6b8861b2923827f39582e
Bryce DesBrisay <bryce@desbrisay.com>
2024-09-10 23:35:14 -0700

    timelapse script timing fix

--- geodash/timelapse/timelapse.sh
+++ geodash/timelapse/timelapse.sh
@@ -11,6 +11,8 @@ input_video=$1
 target_duration=$2
 output_video=$3
 
+# Example usage: ./timelapse.sh input.mp4 60 output.mp4
+
 # Get the original duration of the video in seconds
 original_duration=$(ffmpeg -i "$input_video" 2>&1 | grep "Duration" | cut -d ' ' -f 4 | sed s/,//)
 hours=$(echo $original_duration | cut -d ':' -f 1)
@@ -19,9 +21,12 @@ seconds=$(echo $original_duration | cut -d ':' -f 3)
 original_duration_in_seconds=$(echo "$hours*3600 + $minutes*60 + $seconds" | bc)
 
 # Calculate the speed factor
-speed_factor=$(echo "$original_duration_in_seconds / $target_duration" | bc -l)
+speed_factor=$(echo "$target_duration / $original_duration_in_seconds" | bc -l)
 
-# Use ffmpeg to speed up the video
-ffmpeg -i "$input_video" -filter_complex "[0:v]setpts=${speed_factor}*PTS[v];[0:a]atempo=1/${speed_factor}[a]" -map "[v]" -map "[a]" "$output_video"
+echo "Original duration in seconds: $original_duration_in_seconds"
+echo "Speed factor: $speed_factor"
+
+# Use ffmpeg to make the video the target duration
+ffmpeg -i "$input_video" -filter:v "setpts=${speed_factor}*PTS" -an "$output_video"
 
 echo "Video has been sped up and saved to $output_video"
